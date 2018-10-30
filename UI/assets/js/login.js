14638080a69bd33d2c4a4ef64bf2a81486b964b5
kevinene91 <kevinene91@gmail.com>
2018-10-30 17:57:23 +0300

    [Feature #161585067] add comments

--- UI/assets/js/login.js
+++ UI/assets/js/login.js
@@ -1,18 +1,21 @@
+// get the button to submit the query 
 var submit = document.getElementById("submit")
+//  add eventListener on the button
 submit.addEventListener('click', loginFunction)
 
+// implement the call back function
 function loginFunction(e){
 	e.preventDefault()
 	var email = document.getElementById("email").value 
 	var password = document.getElementById("password").value
 
-
+	//  the data to post 
 	var data = {
 	email:email,
 	password:password,
 	};
 
-
+	//  post the data to via fetch
 	fetch("https://store-manger.herokuapp.com/api/v2/auth/login",{
 	headers: {
 		'Content-Type': 'application/json',
@@ -27,6 +30,7 @@ function loginFunction(e){
 	.then(function(response){
 		localStorage.setItem('access_token', response.access_token)
 		if (response.message == "logged in"){
+			// redirect to dashboard
 			window.location.href = 'UI/templates/dashboard.html'
 		}
 	})
